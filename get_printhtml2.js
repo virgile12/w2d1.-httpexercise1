29b49fe10cd76b1e7e2a8f5c1b29778ee9122b50
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-11 20:58:04 +0000

    first step exercise

---
+++ get_printhtml2.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks(options) {
+
+
+    https.get(options, function (response) {
+     var output = '';
+     response.setEncoding('UTF8');
+     response.on('data', function(chunk) {
+        output += chunk;
+        console.log(output)
+        });
+    })
+}
+
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+  getAndPrintHTMLChunks(requestOptions);
