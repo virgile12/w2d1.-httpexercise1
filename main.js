e74a3b7ad3a1f6594d40e47a87180c3fae3d1500
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-11 17:06:26 +0000

    first update http read and chunks

--- main.js
+++ main.js
@@ -22,19 +22,3 @@ function getAndPrintHTMLChunks() {
 }
   
 getAndPrintHTMLChunks();
-
-// var withBreaks = "Hello World. \n My name is Jennifer. \n What is your name?" 
-
-// var callback = function(response) {
-//     console.log('In response handler callback!');
-  
-//     response.on('data', function(chunk) {
-//       console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-//       console.log(chunk.toString());
-//     });
-//   }
-//   console.log("I'm about to make the request!");
-  
-//   https.request(options, callback).end();
-  
-//   console.log("I've made the request!");
